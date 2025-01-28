a8f9530274fac442829e5a5f2a722a1850506cd7
Abdurahman Omoruyi <abdurahmanomoruyi1@gmail.com>
2025-01-28 12:02:41 -0600

    ------

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -12,16 +12,19 @@ const validateURL = (value, helpers) => { // what is helpers doing
     return helpers.error('string.uri');
 }
 
-const validateClothingItem = celebrate({ // other context 
+const validateClothingItem = celebrate({
     body: Joi.object().keys({
-        name: Joi.string().min(2).max(30).required(),
-        weather: Joi.string().valid('hot', 'warm', 'cold').required(),
-        imageUrl: Joi.string().required().custom(validateURL).messages({
-            'string.empty': 'The "imageUrl" field must be filled in',
-            'string.uri': 'the "imageUrl" field must be a valid url',
-        }),
-    })
-});
+      _id: Joi.string().optional(),
+      owner: Joi.string().optional(),
+      name: Joi.string().min(2).max(30).required(),
+      weather: Joi.string().valid('hot', 'warm', 'cold').required(),
+      imageUrl: Joi.string().required().custom(validateURL).messages({
+        'string.empty': 'The "imageUrl" field must be filled in',
+        'string.uri': 'The "imageUrl" field must be a valid URL',
+      }),
+    }),
+  });
+  
 
 const validateUserInfo = celebrate({
     body: Joi.object().keys({
