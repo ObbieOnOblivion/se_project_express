116132ff6bace199a5641cf6c09ef252302f7f51
Abdurahman Omoruyi <abdurahmanomoruyi1@gmail.com>
2025-01-28 13:11:12 -0600

    -------

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -11,17 +11,22 @@ const validateURL = (value, helpers) => { // what is helpers doing
     }
     return helpers.error('string.uri');
 }
-
-const validateClothingItem = celebrate({ // other context 
+const validateClothingItem = celebrate({
     body: Joi.object().keys({
-        name: Joi.string().min(2).max(30).required(),
-        weather: Joi.string().valid('hot', 'warm', 'cold').required(),
-        imageUrl: Joi.string().required().custom(validateURL).messages({
-            'string.empty': 'The "imageUrl" field must be filled in',
-            'string.uri': 'the "imageUrl" field must be a valid url',
-        }),
-    })
-});
+      _id: Joi.alternatives().try(
+        Joi.string().pattern(/^[0-9a-fA-F]{24}$/), // MongoDB ObjectId
+        Joi.number().integer(), // Integer ID
+        Joi.string().uuid() // UUID
+      ).optional(),
+      owner: Joi.string().pattern(/^[0-9a-fA-F]{24}$/).optional(),
+      name: Joi.string().min(2).max(30).required(),
+      weather: Joi.string().valid('hot', 'warm', 'cold').required(),
+      imageUrl: Joi.string().required().custom(validateURL).messages({
+        'string.empty': 'The "imageUrl" field must be filled in',
+        'string.uri': 'The "imageUrl" field must be a valid URL',
+      }),
+    }),
+  });
 
 const validateUserInfo = celebrate({
     body: Joi.object().keys({
